169a51605b42ccd48a0fde9d50f98fdde422b867
Renilson Meneguci <renilson.meneguci@crivelli.com.br>
2016-03-24 11:04:56 -0300

    Asp.net MVC Identity Sample

---
+++ FormIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FormIdentity.Startup))]
+namespace FormIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
